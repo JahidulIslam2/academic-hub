1d0544de5d1aa8a8b2cfce2a5fe6219f1431600d
Jahidul Islam <jahid2111@gmail.com>
2022-09-30 23:15:22 +0600

    add new Option

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -54,14 +54,14 @@ const Home = () => {
             </div>
             {/* Question */}
             <div>
-                <h1>how Work React..?</h1>
+                <h1 className='font-extrabold'>how Work React..?</h1>
                 <p> React implements a virtual DOM that is basically a DOM tree representation in JavaScript.So when it needs to read or write to the DOM, it will use the virtual representation of it. Then the virtual DOM will try to find the most efficient way to update the browser's DOM.</p>
 
                 <p>
                     <h1>Difference by props and State</h1>
                     Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.
                 </p>
-                <h1>why use useEffect other than data load..?</h1>
+                <h1 className='font-extrabold'>why use useEffect other than data load..?</h1>
                 <p>
                     useEffect hook Some Use case.Running on state change: validating input field.live filtering. trigger animation on new array value.update paragraph list on fetched API data update.
                 </p>
